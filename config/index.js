4f25c08b73cea8d1d4ee11621ce80de654b1cba1
曾志刚 <zengzg@paraview.cn>
2020-06-14 16:12:31 +0800

    add

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/proxyApi': {
-        target: 'http://127.0.0.1/blog',//设置你调用的接口域名和端口号 别忘了加http
+        target: 'http://127.0.0.1:8086/blog',//设置你调用的接口域名和端口号 别忘了加http
         changeOrigin: true,//如果需要跨域
         pathRewrite: {
           '^/proxyApi': '',//调用接口直接写‘/api/user/add’即可
@@ -21,7 +21,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
